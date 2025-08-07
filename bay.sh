4b74cbeb09348b9cd8961e1d5e3b7c20d42a7adb
AKHMAT <xxxtentachion177@gmail.com>
2025-08-07 11:18:59 +0300

    project created

---
+++ bay.sh
@@ -0,0 +1,4 @@
+#\!/bin/bash
+rm ./day1/hello.txt ./day2/hello.txt
+rmdir day1 day2
+
---
+++ hello.sh
@@ -0,0 +1,8 @@
+#\!/bin/bash
+mkdir day1 day2 day3
+touch ./day1/hello.txt
+echo "Hello school 21" > ./day1/hello.txt
+cp ./day1/hello.txt ./day2
+rmdir day3
+
+
